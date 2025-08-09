8a9f881a48a424958b7f0ab3a4796cce0f8c45dd
missionsave <foundationmissionsave@gmail.com>
2025-08-09 01:38:00 +0100

    improvements

--- common/fl_scintilla.hpp
+++ common/fl_scintilla.hpp
@@ -39,7 +39,7 @@ struct FileEntry {
 };
  struct fl_scintilla : public Fl_Scintilla {  
     std::string filename="";
-	string floaded="";
+	std::sstring floaded="";
     sptr_t curr_file_pointer=0;
     std::string comment;
     std::unordered_map<sptr_t,uptr_t> filesfirstline;  
