45393d2f37507dc6bca976cb0e3c97d0c063046d
missionsave <foundationmissionsave@gmail.com>
2026-09-21 21:11:06 +0100

    update

--- appcad/lua/molde.lua
+++ appcad/lua/molde.lua
@@ -6,3 +6,12 @@ Rec(10)
 Extrude(10)
 Mloc(10,5,0,-90,-90)
 Pl "0,0 @5,0 @0,5"
+
+Part "study_hsp"
+Rec(1000,600)
+Mloc(1000)
+Rec(490,50)
+Rotatelz(45*3)
+Dup()
+Mloc(653.52,346.48,0)
+Rotatelz(-45*3)
